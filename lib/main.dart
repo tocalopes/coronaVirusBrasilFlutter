22706c35214a5204e0890a8da845e137553bb134
gabriel lopes <gabriellopes112009@gmail.com>
2020-03-21 23:40:53 -0300

    beggining of layout with implementation of country em confirmed cases on Brazil

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  fetchAlbum();
   runApp(
       MaterialApp(
-        home: Container(),
+        home: Myapp(),
       )
   );
 }
@@ -24,11 +23,8 @@ Future<Map> getData() async {
     headers: {"x-rapidapi-host" : "covid-19-coronavirus-statistics.p.rapidapi.com",
       "x-rapidapi-key": "1fce8bb448msh9f8ab63696ca9f0p1ccd4ajsn69f8f8834355"},
   );
-  Map responseJson = json.decode(response.body);
-  return responseJson;
+  return json.decode(response.body);
 }
-
-
 
 
 class Myapp extends StatefulWidget {
@@ -37,25 +33,78 @@ class Myapp extends StatefulWidget {
 }
 
 class _MyappState extends State<Myapp> {
+
+  String country;
+  String lastUpdate;
+  String confirmed;
+  String deaths;
+  String recovered;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Corona Virus"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text(
+                    "carregando dados",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ));
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                    child: Text(
+                      "Falha ao carregar dados :/",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ));
+              } else {
 
+                country = snapshot.data["data"]["covid19Stats"][0]["country"];
+                lastUpdate = snapshot.data["data"]["covid19Stats"][0]["lastUpdate"].toString();
+                confirmed = snapshot.data["data"]["covid19Stats"][0]["confirmed"].toString();
+                deaths = snapshot.data["data"]["covid19Stats"][0]["deaths"].toString();
+                recovered = snapshot.data["data"]["covid19Stats"][0]["recovered"].toString();
 
-/*class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({this.userId, this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.help_outline,
+                          size: 150.0, color: Colors.green),
+                      Text("Pais: $country",style:TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.green,
+                      )),
+                      Text("caso confirmados: $confirmed",style:TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.green,
+                      )),
+                    ],
+                  ),
+                );
+              }
+          }
+        },
+      ),
     );
   }
-}*/
+}
